f3c2961c37924b3b873dd936690d96b1ef8dd6ab
DaryaKhatsuk <112404482+DaryaKhatsuk@users.noreply.github.com>
2022-10-05 11:05:37 +0300

    v.0.7

--- project/Incident_Database/admin.py
+++ project/Incident_Database/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Employees, IncidentDB
 
-# Register your models here.
+
+@admin.register(Employees)
+class EmployeesAdmin(admin.ModelAdmin):
+    list_display = ('certificate', 'full_name', 'title', 'address', 'family_composition')
+    list_filter = ('certificate', 'full_name')
+    search_fields = ('certificate', 'full_name',)
+
+
+@admin.register(IncidentDB)
+class IncidentDBAdmin(admin.ModelAdmin):
+    list_display = ('registration_number', 'date_of_registration', 'type_event', 'source_messages',
+                    'registration_criminal_case', 'number_criminal_case', 'number_of_the_operation',
+                    'registration_number_of_the_employee', 'the_scene_of_the_incident',
+                    'a_brief_description_of_the_incident')
+    list_filter = ('registration_number', 'type_event')
+    search_fields = ('registration_number', 'type_event',)
+
--- project/Incident_Database/models.py
+++ project/Incident_Database/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Employees(models.Model):
+    id_base = models.AutoField(primary_key=True)
+    certificate = models.DecimalField(unique=True, decimal_places=0, max_digits=7, max_length=7,
+                                      verbose_name='Номер удостоверения', help_text='Не более 7 цифр')
+    full_name = models.CharField(max_length=35, verbose_name='Ф.И.О.')
+    title = models.CharField(max_length=20, verbose_name='Звание')
+    address = models.CharField(max_length=30, verbose_name='Адрес')
+    family_composition = models.CharField(max_length=20, verbose_name='Состав семьи')
+
+    def __str__(self):
+        return f'{self.certificate}'
+
+    class Meta:
+        verbose_name = 'Сотрудникa'
+        verbose_name_plural = 'Сотрудники'
+        ordering = ('certificate',)
+
+
+class IncidentDB(models.Model):
+    id_base = models.AutoField(primary_key=True)
+    registration_number = models.DecimalField(unique=True, decimal_places=0, max_digits=7, max_length=7,
+                                              verbose_name='Номер происшествия', help_text='Не более 7 цифр')
+    date_of_registration = models.DateField(verbose_name='Дата происшествия')
+    type_event = models.CharField(max_length=20, verbose_name='Тип происшествия')
+    source_messages = models.CharField(max_length=20, verbose_name='Источник сообщения')
+    registration_criminal_case = models.TextField(verbose_name='Состояние уголовного дела')
+    number_criminal_case = models.DecimalField(unique=True, decimal_places=0, max_digits=7, max_length=7, null=True,
+                                               blank=True, verbose_name='Номер уголовного дела',
+                                               help_text='Не более 7 цифр')
+    number_of_the_operation = models.DecimalField(unique=True, decimal_places=0, max_digits=7, max_length=7,
+                                                  verbose_name='Регистрационный номер сообщения',
+                                                  help_text='Не более 7 цифр')
+    registration_number_of_the_employee = models.ForeignKey(Employees, on_delete=models.DO_NOTHING,
+                                                            verbose_name='Регистрационный номер сотрудника')
+    the_scene_of_the_incident = models.CharField(max_length=20, verbose_name='Место происшествия')
+    a_brief_description_of_the_incident = models.TextField(verbose_name='Краткое описание происшествия')
+
+    def __str__(self):
+        return f'{self.registration_number}'
+
+    class Meta:
+        verbose_name = 'Происшествие'
+        verbose_name_plural = 'Происшествия'
+        ordering = ('registration_number',)
